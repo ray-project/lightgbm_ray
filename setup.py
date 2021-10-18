2a950ca4ef57517e4c4205ef9eda74ae8fa3c289
Antoni Baum <antoni.baum@protonmail.com>
2021-10-18 23:31:01 +0000

    Bump version

--- setup.py
+++ setup.py
@@ -3,10 +3,10 @@ from setuptools import find_packages, setup
 setup(
     name="lightgbm_ray",
     packages=find_packages(where=".", include="lightgbm_ray*"),
-    version="0.1.0",
+    version="0.1.1",
     author="Ray Team",
     description="A Ray backend for distributed LightGBM",
     long_description="A distributed backend for LightGBM built on top of "
     "distributed computing framework Ray.",
     url="https://github.com/ray-project/lightgbm_ray",
-    install_requires=["lightgbm>=3.2.1", "xgboost_ray>=0.1.3"])
+    install_requires=["lightgbm>=3.2.1", "xgboost_ray>=0.1.4"])
