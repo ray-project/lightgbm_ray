f3f41027efb67569b804cced57ab473b28c26076
Antoni Baum <antoni.baum@protonmail.com>
2022-10-06 19:51:29 +0000

    Bump version to 0.1.8
    Signed-off-by: Antoni Baum <antoni.baum@protonmail.com>

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import find_packages, setup
 setup(
     name="lightgbm_ray",
     packages=find_packages(where=".", include="lightgbm_ray*"),
-    version="0.1.7",
+    version="0.1.8",
     author="Ray Team",
     description="A Ray backend for distributed LightGBM",
     license="Apache 2.0",
