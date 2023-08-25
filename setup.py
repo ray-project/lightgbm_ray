47fe3fc87f861cb6f182064557a2b76981fae749
Kai Fricke <krfricke@users.noreply.github.com>
2023-08-25 17:12:45 +0200

    Bump to 0.1.10 (#51)
    Signed-off-by: Kai Fricke <kai@anyscale.com>

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import find_packages, setup
 setup(
     name="lightgbm_ray",
     packages=find_packages(where=".", include="lightgbm_ray*"),
-    version="0.1.9",
+    version="0.1.10",
     author="Ray Team",
     description="A Ray backend for distributed LightGBM",
     license="Apache 2.0",
